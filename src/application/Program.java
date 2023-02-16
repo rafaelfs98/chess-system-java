f67114c619368c8f90056b6e7e1edd4c5616a484
rafaelfs98 <rafaelfs98@outlook.com>
2023-02-16 13:31:24 -0300

    First class : Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("test 1");
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
 
     }
 }
